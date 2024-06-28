fa35c7f36f174d20789a848b5a1da5d0e9cf0c1a
Bhaskara G Badiger <57971971+BhaskaraGB@users.noreply.github.com>
2024-06-28 15:05:56 -0500

    Add files via upload

---
+++ 02-mapping/bwa_mem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#activate bwa envrionment in conda 
+#NOTE: To use BWA, you need to first index the genome with `bwa index'.
+
+# Set the directories and files
+TRIMMED_DIR="/Users/bhaskara/Documents/PH_Stress_2019_Tagseq/01-qual-filter/qual_filter_files"
+OUTPUT_DIR="BWA_Mapping"
+REFERENCE_GENOME="/Users/bhaskara/Documents/Reference_Annotation/PH_HAL_REF/PhalliiHAL_591_v2.0.softmasked.fa"
+
+# Create output directory if it does not exist
+mkdir -p "$OUTPUT_DIR"
+
+# Run bwa mem on each file in the trimmed directory
+for fastq_file in "$TRIMMED_DIR"/*_concat_clean.fastq; do
+    # Extract the base name of the file (without directory and extension)
+    base_name=$(basename "$fastq_file" _concat_clean.fastq)
+    
+    # Run bwa mem to produce the SAM file
+    bwa mem -M -t 10 "$REFERENCE_GENOME" "$fastq_file" > "$OUTPUT_DIR/${base_name}.sam"
+    
+    echo "Processed $fastq_file: output saved to $OUTPUT_DIR/${base_name}.sam"
+done
+
+echo "BWA MEM alignment complete. SAM files are located in $OUTPUT_DIR."
