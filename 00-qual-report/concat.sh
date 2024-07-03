cd0253b7bff018d653c484509100d1e1e9fad5b8
Bhaskara G Badiger <57971971+BhaskaraGB@users.noreply.github.com>
2024-07-03 17:24:11 -0500

    Update concat.sh

--- 00-qual-report/concat.sh
+++ 00-qual-report/concat.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Purpose of the Script
-#The script is designed to:
-
 # 1. Extract sample names from a metadata file (meta_noheader).
 # 2. Find FASTQ files corresponding to these sample names in the specified input directory (fastq_files).
 # 3. Concatenate these FASTQ files and store the result in an output directory (concat_files).
